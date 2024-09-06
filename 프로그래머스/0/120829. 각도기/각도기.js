ae1efd47588155d0dbf24f48f0a6dc33a825f1a2
pse11 <102156161+pse11@users.noreply.github.com>
2024-09-06 23:53:48 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -4,7 +4,7 @@ function solution(angle) {
         result=1
     } else if(angle===90){
         result=2
-    } else if(angle>90 &&angle<180){
+    } else if(angle<180){
         result=3
     }else if(angle===180){
         result=4
