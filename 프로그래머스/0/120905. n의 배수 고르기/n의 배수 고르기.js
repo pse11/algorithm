e72c5e152b0d303ee241a46f3430d3e670794ec7
pse11 <102156161+pse11@users.noreply.github.com>
2024-09-07 21:25:11 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    let result = []
-    for(data of numlist){
-        if(data%n===0){
-            result.push(data)
-        }
-    }
-    return result
+    return numlist.filter((value)=> value%n===0)
 }
