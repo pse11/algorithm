0129e3a6237d6f1d905bec288811e407dfbde5d2
pse11 <102156161+pse11@users.noreply.github.com>
2024-09-07 21:04:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,11 +1,4 @@
 function solution(array) {
-    let max = array[0]
-    let index = 0
-    for(const i in array){
-       if(max<array[i]){
-           max=array[i]
-           index = i
-       }
-    }
-    return [max,parseInt(index)]
+    const max = Math.max(...array)
+    return [max, array.indexOf(max)]
 }
