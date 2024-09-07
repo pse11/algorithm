aefd152ee4fcbe4c256678aeb305800557ed1711
pse11 <102156161+pse11@users.noreply.github.com>
2024-09-07 22:06:17 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-    let result = []
-    for(let i=0;i<my_string.length;i++){
-        if(!isNaN(my_string[i])){
-            result.push(parseInt(my_string[i]))
-        }
-    }
-    return result.sort()
+    return my_string.split('').filter((value)=>!isNaN(value)).map((value)=>parseInt(value)).sort()
 }
