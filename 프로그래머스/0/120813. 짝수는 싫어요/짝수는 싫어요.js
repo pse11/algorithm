c0ec609e13b965b7eabe1748b74557606ecbb86f
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-01-28 23:40:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    let array =[]
-    for(let i=1;i<=n;i++){
-        if(i%2==1){
-            array.push(i)
-        }
+    let result = [];
+    for(let i=1;i<=n;i+=2){
+        result.push(i);
     }
-    return array
+    return result;
 }
