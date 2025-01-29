c7cdbc496c287d8b72dbc6d2808ed03d3ea8ba76
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-01-29 16:18:18 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let result =0;
-    if((n/7)%1===0){
-        result=n/7;
-    }else {
-        result = Math.floor(n/7)+1;
-    }
-    return result;
+    return Math.ceil(n/7);
 }
