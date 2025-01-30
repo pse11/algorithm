3f04b969a5080f853d03fc3976dbb2a7134a871e
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-01-30 23:04:08 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,13 +1,11 @@
 function solution(price) {
-    let result=0
-    if(price>=100000&&price<300000){
-        result=price*0.95
+    let result=price;
+    if(price<300000&&price>=100000){
+        result=price*0.95;
     }else if(price>=300000&&price<500000){
-        result=price*0.9
+        result= price*0.9;
     }else if(price>=500000){
-        result=price*0.8
-    }else {
-        result=price
+        result=price*0.8;
     }
-    return parseInt(result)
+    return Math.floor(result);
 }
