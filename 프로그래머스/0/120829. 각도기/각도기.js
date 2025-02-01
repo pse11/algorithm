b9ba3bcd5bc9a294dc06687398da6ffa7a968cfc
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-02-01 23:22:44 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,13 +1,13 @@
 function solution(angle) {
-    let result
-    if(angle>0 && angle<90){
-        result=1
-    } else if(angle===90){
-        result=2
-    } else if(angle<180){
-        result=3
-    }else if(angle===180){
-        result=4
+    let result=0;
+    if(angle>0&&angle<90){
+        result=1;
+    }if(angle===90){
+        result=2;
+    }if(angle>90&&angle<180){
+        result=3;
+    }if(angle===180){
+        result=4;
     }
-    return result
-}
+    return result;
+} 
