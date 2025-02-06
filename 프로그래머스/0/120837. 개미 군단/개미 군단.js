77e498fb3770f3b32850ae9777b6b3b153808d43
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-02-06 23:38:10 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,11 +1,3 @@
 function solution(hp) {
-    let array=[]
-    array[0]=Math.floor(hp/5)
-    array[1]=Math.floor(hp%5/3)
-    array[2]=Math.floor(hp%5%3)
-    let sum=0
-    for(let i=0;i<3;i++){
-        sum+=array[i]
-    }
-    return sum
+    return Math.floor(hp/5)+Math.floor((hp%5)/3)+Math.floor((hp%5)%3);
 }
