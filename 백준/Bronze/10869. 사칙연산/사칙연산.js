f13b369ad6c44cf86c8d9ab51ab828441ca4ee77
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-10 11:58:09 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(parseInt(A/B));
+console.log(A%B);
