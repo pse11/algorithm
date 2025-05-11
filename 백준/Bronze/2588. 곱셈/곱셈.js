e245fce6d5db880934513a0ff2aee861bb92f8d6
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-11 11:14:11 +0900

    [Bronze III] Title: 곱셈, Time: 92 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -2,14 +2,12 @@ let fs = require('fs');
 let input = fs.readFileSync('dev/stdin').toString().split('\n');
 
 let A = input[0].trim();
-let B = input[1].trim().split("");
-let result=0;
+let B = input[1].trim();
 
 for(let i=B.length-1;i>=0;i--){
-  let num =A*B[i]*(10**(B.length-i-1));
   console.log(A*B[i]);
-  result+=num;
 }
 
-console.log(result);
+console.log(A*B);
+
 
