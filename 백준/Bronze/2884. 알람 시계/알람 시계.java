3bcd9eada1917531098e392c8643c6893ca7f49b
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-16 23:17:58 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int H = input.nextInt();
-        int M = input.nextInt();
-        
-        if(M<45){
-            H-=1;
-            M=60+M-45;
-            if(H<0){
-                H=23;
-            }
-        }else{
-            M=M-45;
-        }
-        
-        System.out.println(H+" "+M);
-        
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int h = sc.nextInt();
+    int m = sc.nextInt();
+    if(m>=45){ //분이 45분보다 클때
+      m-=45;
+    }else{ // 분이 45분보다 작을때 시에서 분을 가져온다. 단 0시일 경우 23시로 변경해줘야한다.
+      if(h==0){
+        h=23;
+      }else{
+        h--;
+      }
+      m=m+60-45;
     }
+    System.out.println(h+" "+m);
+  }
 }
