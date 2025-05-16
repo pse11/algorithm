54f3ef47a889f1f6bb29682ad98e4647a7a536d3
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-17 00:04:14 +0900

    [Bronze V] Title: 합, Time: 164 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        
-        int n = input.nextInt();
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum+=(i+1);
-        }
-        System.out.println(sum);
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum =0;
+    for(int i=1;i<=n;i++){
+      sum+=i;
     }
+    System.out.println(sum);
+  }
 }
