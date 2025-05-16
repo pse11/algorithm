97312ce752973a92b019d6f0d10b9cea50e25edf
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-17 00:07:58 +0900

    [Bronze IV] Title: 영수증, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int X = input.nextInt();
-        int N = input.nextInt();
-        int sum = 0;
-        
-        for(int i=0; i<N;i++){
-            int a = input.nextInt();
-            int b = input.nextInt();
-            sum+=(a*b);
-        }
-        if(sum==X){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
-        }
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int X = sc.nextInt();
+    int N = sc.nextInt();
+    int sum = 0;
+    for(int i=0;i<N;i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      sum+=(a*b);
     }
+    if(sum==X){
+      System.out.println("Yes");
+    }else{
+      System.out.println("No");
+    }
+  }
 }
