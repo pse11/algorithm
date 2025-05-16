62b7a24ffe50d28a6461dae14ee48c8a120af65d
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-16 23:48:55 +0900

    [Bronze V] Title: 구구단, Time: 180 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main{
- public static void main(String[] args){
-     Scanner input = new Scanner(System.in);
-     
-     int N=input.nextInt();
-     
-     for(int i=1;i<10;i++){
-         System.out.println(N +" * "+i +" = "+N*i);
-     }
- }   
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for(int i=1;i<=9;i++){
+      System.out.printf("%d * %d = %d",N,i,N*i);
+      System.out.println();
+    }
+
+  }
 }
