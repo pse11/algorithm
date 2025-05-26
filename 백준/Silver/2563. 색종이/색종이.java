0f8bdfbc85d1d664b89752324e9ec0658479f551
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-05-27 00:31:32 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -12,7 +12,7 @@ public class Main{
       StringTokenizer st = new StringTokenizer(br.readLine());
       int x=Integer.parseInt(st.nextToken());
       int y =Integer.parseInt(st.nextToken());
-
+        //사각형을 종이에 한칸씩 채워넣는다.
       for(int j=x;j<x+10;j++){
         for(int k=y;k<y+10;k++){
           arr[j][k]=1;
@@ -21,7 +21,7 @@ public class Main{
     }
     int sum =0;
     for(int i=0;i<100;i++){
-      for(int j=0;j<100;j++){
+      for(int j=0;j<100;j++){ //종이가 1로 채워져있는 경우를 더해 면적을 구한다.
         if(arr[i][j]==1){
           sum+=arr[i][j];
         }
