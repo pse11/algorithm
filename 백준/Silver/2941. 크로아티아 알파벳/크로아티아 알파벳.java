82273611a2c79838c005258c82e2d976c157bc81
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-06 22:56:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -9,10 +9,11 @@ public class Main{
     int count =0;
     //문자열을 한글자씩 탐색하면서 c,d,l,n,s,z를 만나면 그 뒤의 인덱스까지 탐색하면서 갯수 추가
     for(int i=0;i<input.length();i++){
-          
       char ch = input.charAt(i);
       count++;
       if(ch=='c'){
+        //c가 마지막 인덱스에 위치에 있으면 i+1에서 인덱스에러가 나기때문에 
+        //c가 마지막 인덱스 이전에 위치해있는 경우에 탐색한다.
         if(i<input.length()-1){
           if(input.charAt(i+1)=='='||input.charAt(i+1)=='-'){
             i++;
