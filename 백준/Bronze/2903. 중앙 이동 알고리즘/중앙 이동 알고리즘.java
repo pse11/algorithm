86cb331a9f382d316df5e50ba637b163221f9e92
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-07 22:40:26 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -9,16 +9,15 @@ public class Main{
     //규칙
     /* 첫번째 줄의 점 개수의 제곱
      * 초기 2제곱
-     * 1번 (2+1)제곱
-     * 2번 (3+2)제곱
-     * 3번 (5+4)제곱
-     * 4번 (9+8)제곱
-     * 규칙 : (앞의 결과 + 2의 (번수 -1)승)의 제곱
+     * 1번 3제곱
+     * 2번 5제곱
+     * 3번 9제곱
+     * 규칙 : (2의 n승 +1) 제곱
      */
 
     int first = 2;
     for(int i=0;i<N;i++){
-      first = (first+ (int)Math.pow(2,i));
+      first = (int)Math.pow(2,i+1)+1;
     }
     bw.write((int)Math.pow(first,2)+"");
     bw.close();
