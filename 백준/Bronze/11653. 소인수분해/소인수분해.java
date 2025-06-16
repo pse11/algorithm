7387e05b9eeaae38f54ea5ea1413e0e1334e332f
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-16 16:01:36 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 		int N = sc.nextInt();
 
 		if(N!=1){
 			for(int i=2;i<=N;i++){
 				while(N%i==0){
 					N /=i;
-					sb.append(i+"\n");
+					System.out.println(i);
 				}
 			}
 		}
-		System.out.println(sb);
+		
 	}	
 }
