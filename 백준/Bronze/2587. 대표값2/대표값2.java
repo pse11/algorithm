a2aa9d9cea2805916c7e306f9c69e3691e77f425
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-22 20:46:32 +0900

    [Bronze II] Title: 대표값2, Time: 164 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i=0;i<5;i++){
+          arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<4;i++){
+          for(int j=i+1;j<5;j++){
+            if(arr[i]>arr[j]){
+              int tmp = 0;
+              tmp=arr[i];
+              arr[i]=arr[j];
+              arr[j]=tmp;
+            }
+          }
+        }
+        int sum = 0;
+        for(int i=0;i<5;i++){
+          sum+=arr[i];
+        }
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+  }
+}
