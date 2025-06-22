08071e3abe16491eb6969a37dff20de173b08ae5
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-22 15:56:58 +0900

    [Bronze III] Title: 네 번째 점, Time: 184 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int[] a = {sc.nextInt(), sc.nextInt()};
+      int[] b = {sc.nextInt(), sc.nextInt()};
+      int[] c = {sc.nextInt(), sc.nextInt()};
+      int x=0,y=0;
+      if(a[0]==b[0]){
+        x = c[0];
+      }else if(a[0]==c[0]){
+        x=b[0];
+      }else{
+        x=a[0];
+      }
+      if(a[1]==b[1]){
+        y = c[1];
+      }else if(a[1]==c[1]){
+        y=b[1];
+      }else{
+        y=a[1];
+      }
+      System.out.println(x+" "+y);
+  }
+}
