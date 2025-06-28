1a74fd1013c7657f7be5456d89d141a0f12a4be8
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-29 00:39:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count =0;
+    System.out.println((long)n*(n-1)/2);
+    System.out.println(2);
+  }
+}
