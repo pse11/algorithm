93cc0854643d0c9d35ff935995c1b279707c7e60
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-06-30 18:15:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1264 ms, Memory: 117372 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,19 @@
-import java.util.StringTokenizer;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+
 public class Main{
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int n = Integer.parseInt(br.readLine());
-    List<Integer> arr = new ArrayList<>();
+    int[] arr = new int[n];
     for(int i=0;i<n;i++){
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      arr.add(Integer.parseInt(st.nextToken()));
+      arr[i]=Integer.parseInt(br.readLine());
     }
 
-    Collections.sort(arr);
+    Arrays.sort(arr);
     for(int i=0;i<n;i++){
-      bw.write(arr.get(i)+"\n");
+      bw.write(arr[i]+"\n");
     }
     bw.close();
   }
