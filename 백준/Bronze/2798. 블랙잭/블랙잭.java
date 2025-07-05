9365afae64d21d31759bdd3ccab4c2c82b2ac933
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-07-05 22:06:06 +0900

    [Bronze II] Title: 블랙잭, Time: 148 ms, Memory: 18760 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Main{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int[] arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0;i<N;i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int sum = 0;
+    List<Integer> list = new ArrayList<>();
+    for(int i=0;i<N;i++){
+      for(int j=i+1;j<N;j++){
+        for(int k=j+1;k<N;k++){
+          sum=arr[i]+arr[j]+arr[k];
+          if(sum<=M){ //M보다 작은 sum인경우 조건은 만족하므로 
+            list.add(sum); //list에 추가해준뒤, max로 최대한 M에 가까운 합 출력
+          }
+        }
+      }
+    }
+    bw.write(Collections.max(list)+"");
+    bw.close();
+  }
+}
