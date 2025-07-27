a07e59788fd5d5041eef9597ccd00f2de6333baa
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-07-27 18:03:31 +0900

    [Silver IV] Title: 스택 2, Time: 4288 ms, Memory: 256188 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,12 +1,43 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main{
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+    //java의 stack 클래스로 구현
+    Stack<Integer> stack = new Stack<>();
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = null;
+    for(int i=0;i<N;i++){
+      st = new StringTokenizer(br.readLine());
+      //명령 입력
+      int command = Integer.parseInt(st.nextToken());
+      int res = 0;
+      switch (command) {
+        case 1:
+          int x = Integer.parseInt(st.nextToken());
+          stack.push(x);
+          break;
+        case 2:
+          res = stack.isEmpty()?-1:stack.pop(); //pop : 스택의 맨 위요소를 제거하고 반환
+          System.out.println(res);
+          break;
+        case 3:
+          System.out.println(stack.size());
+          break;
+        case 4:
+          res = stack.isEmpty()?1:0;
+          System.out.println(res);
+          break;
+        case 5:
+          res = stack.isEmpty()?-1:stack.peek(); //peek: 스택의 맨 위 요소를 반환하지만 제거는 하지 않음
+          System.out.println(res);
+          break;
+      }
+    }
+    /*
+     *  List로 스택 구현
     //명령 수 N 입력
     int N = Integer.parseInt(br.readLine());
     //stack 선언
@@ -23,9 +54,9 @@ public class Main{
           break;
         case 2:
           if(stack.size()==0){
-            System.out.println("-1");
+            System.out.println(-1);
           }else{
-            System.out.println(stack.get(stack.size()-1)+"");
+            System.out.println(stack.get(stack.size()-1));
             stack.remove(stack.size()-1);
           }
           break;
@@ -38,12 +69,13 @@ public class Main{
           break;
         case 5:
           if(stack.size()==0){
-            System.out.println("-1");
+            System.out.println(-1);
           }else{
             System.out.println(stack.get(stack.size()-1));
           }
           break;
       }
     }
+      */
   }
 }
