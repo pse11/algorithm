893de6635cb9b1e9c5a2c6dd1a32c9f967439c8f
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-08-10 15:19:56 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -4,22 +4,32 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	static long[] arr=new long[21]; //팩토리얼 값을 저장할 배열, arr[i]는 i! 값을 저장
+									//0~20까지 팩토리얼 저장
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		
 		System.out.println(factorial(N));
 		
-		//13!까지만 int형 가능.
-		//20!까지는 long형 가능.
-		//그 이상일 땐 BigInteger
 	}
+	//재귀
+//	public static long factorial(int N) {
+//		if(N<=1) {
+//			return 1;
+//		}
+//		return N*factorial(N-1);
+//	}
+	//메모이제이션
 	public static long factorial(int N) {
-		if(N<=1) {
+		if(N==0||N==1) {
 			return 1;
 		}
-		return N*factorial(N-1);
+		if(arr[N]!=0) {//이미 계산된 값이 있으면 바로 리턴
+			return arr[N];
+		}
+		arr[N] = N*factorial(N-1); //재귀 호출하면서 계산 후 저장
+		return arr[N];
 	}
-
 }
