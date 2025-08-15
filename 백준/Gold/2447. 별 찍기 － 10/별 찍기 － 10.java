0e22465de49c9101b9d8bad4cba6104cda1d4787
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-08-15 16:36:56 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 284 ms, Memory: 47996 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,55 +1,64 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+	
 	static char[][] arr;
- 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
- 
-		arr = new char[N][N];
-        
-		star(0, 0, N, false);
- 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		arr = new char[N][N];
+		
+		star(0,0,N,false);
+		
+		//결과 출력
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
 				sb.append(arr[i][j]);
 			}
-			sb.append('\n');
+			sb.append("\n");
 		}
-		System.out.print(sb);
-	}
- 
-	static void star(int x, int y, int N, boolean blank) {
- 
-		if (blank) {
-			for (int i = x; i < x + N; i++) {
-				for (int j = y; j < y + N; j++) {
-					arr[i][j] = ' ';
+		System.out.println(sb);
+}
+	//x,y : 현재 찍을 구역의 왼쪽 위 좌표, N: 현재 구역의 크기(정사각형 한 변), blank: 현재 구역이 공백으로 채워질지 여부
+	public static void star(int x, int y, int N, boolean blank) {
+		//현재 구역이 공백칸일 경우
+		if(blank) { //blank=true인 구역은 전부 공백으로 채움.더 이상 재귀하지 않고 return
+			for(int i=x;i<x+N;i++) {
+				for(int j=y;j<y+N;j++) {
+					arr[i][j]=' ';
 				}
 			}
 			return;
 		}
-		if (N == 1) {
-			arr[x][y] = '*';
+		//재귀 종료 : 가장 작은 단위 (1x1)일때 별 찍기
+		//N=1이면 더 쪼갤 수 없음
+		if(N==1) {
+			arr[x][y]='*';
 			return;
 		}
-
- 
-		int size = N / 3;
+		//
+		int nextN = N/3;
 		int count = 0;
-		for (int i = x; i < x + N; i += size) {
-			for (int j = y; j < y + N; j += size) {
+		//9개 구역으로 나누어서 재귀 호출
+		for(int i=x;i<x+N;i+=nextN) {
+			for(int j=y;j<y+N;j+=nextN) {
 				count++;
-				if (count == 5) { // 공백 칸일 경우
-					star(i, j, size, true);
-				} else {
-					star(i, j, size, false);
+				if(count==5) { //5번째 칸이 가운데 영역이기 때문에 blank=true
+					star(i,j,nextN,true);
+				}else {
+					star(i,j,nextN,false);
 				}
+				
 			}
 		}
+		
 	}
 }
