d28a946f91de3fbad87c724a8eed2a70754032eb
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-08-16 23:49:08 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 124 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -6,19 +6,20 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		
 		int N = Integer.parseInt(br.readLine());
-		Set<String> set = new HashSet<>(); //무지개 댄스를 추는 사람들 모음 set
+		Set<String> set = new HashSet<>();
 		set.add("ChongChong");
+		StringTokenizer st = null;
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			String A = st.nextToken();
 			String B = st.nextToken();
-			if(set.contains(A)||set.contains(B)) { //set에 A나 B가 있다면 무지개 댄스를 추는 사람이기 때문에 둘 다 set에 add해준다.
+			
+			if(set.contains(A)||set.contains(B)) {
 				set.add(A);
 				set.add(B);
 			}
