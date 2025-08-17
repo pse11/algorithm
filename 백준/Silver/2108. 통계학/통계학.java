9db264c6f7406922ebe40e38d3f10d49b8eb77b1
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-08-17 13:23:05 +0900

    [Silver II] Title: 통계학, Time: 460 ms, Memory: 47632 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,64 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+		
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine()); //입력 개수 N
-		
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		int sum = 0; //총 합계(산술평균 계산용)
+		int sum = 0;
+		
 		for(int i=0;i<N;i++) {
 			int k = Integer.parseInt(br.readLine());
-			arr[i] = k;
-			sum+=k;
+			arr[i]= k;
+			sum += k;
 		}
 		
-		Arrays.sort(arr);	//중앙값, 범위, 최빈값 계산에 필요
+		//산술평균
+		sb.append(Math.round((double)sum/N)).append("\n");
 		
-		//최빈값 계산에 필요한 변수들
-		int count = 0;	//동일한 값이 연속으로 나타날 때 증가시킬 count
-		int max = Integer.MIN_VALUE;	//최빈값의 최대갯수
+		//중앙값
+		Arrays.sort(arr);
+		sb.append(arr[N/2]).append("\n");
 		
-		int mod = arr[0];	//최빈값
-		boolean check = false;	//값이 첫등장인지 체크. 첫 등장하면 true, 아니면 false
+		//최빈값
+		int count = 0;
+		int max = Integer.MIN_VALUE;
 		
-		//최빈값 구하기
+		int mod = arr[0];
+		boolean check = false;
+		
 		for(int i=0;i<N-1;i++) {
-			if(arr[i]==arr[i+1]) { //같은 값이 나타나면 count 증가
+			if(arr[i]==arr[i+1]) {
 				count++;
-			}else {		//값이 다르면 count 초기화
+			}else {
 				count=0;
 			}
-			
-			//새로운 최빈값 등장
-			if(max<count) { 
+			if(max<count) {
 				max=count;
 				mod=arr[i];
 				check=true;
 			}
-			//최빈값 개수가 같고, 첫 등장 이후라면 두 번째 최빈값 저장
+			
 			else if(max==count&&check==true) {
 				mod=arr[i];
-				check=false;	//이후에 최빈값 개수가 같은 값이 등장해도 false이기 때문에 만족하는 조건이 없어서 통과함
+				check=false;
 			}
 		}
-		//산술평균
-		System.out.println(Math.round((double)sum/N));
-		//중앙값
-		System.out.println(arr[N/2]);
-		//최빈값
-		System.out.println(mod);
+		sb.append(mod).append("\n");
 		//범위
-		System.out.println(arr[N-1]-arr[0]);
+		sb.append(arr[N-1]-arr[0]).append("\n");
+		
+		System.out.println(sb);
 	}
 }
