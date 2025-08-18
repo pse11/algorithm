aae367e18d208625ca4a599b62dbf03124bc8da2
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-08-18 22:50:50 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 740 ms, Memory: 42896 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -9,43 +9,39 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+		
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		//단어의 개수
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		//단어 길이 기준
 		int M = Integer.parseInt(st.nextToken());
-		Map<String, Integer> map = new HashMap<>();
+		
+		Map<String,Integer> map = new HashMap<>();
+		
 		for(int i=0;i<N;i++) {
-			String word = br.readLine();
-			if(word.length()>=M) {
-				map.put(word, map.getOrDefault(word, 0)+1); //처음 나온 단어는 1 저장,그 이후에는 1씩 증가
+			String str = br.readLine();
+			if(str.length()>=M) {
+				map.put(str, map.getOrDefault(str, 0)+1);
 			}
 		}
 		List<String> list = new ArrayList<>(map.keySet());
-		//단어의 길이로 정렬
-		Comparator<String> comparator = new Comparator<String>(){
+		Comparator<String> comparator = new Comparator<String>() {
 			@Override
 			public int compare(String s1, String s2) {
-				//자주 나오는 단어를 앞에 배치
-				//map에 저장된 나온 횟수로 비교
-				if(Integer.compare(map.get(s1), map.get(s2))!=0) {
-					return Integer.compare(map.get(s2),map.get(s1));
+				if(Integer.compare(map.get(s1),map.get(s2))!=0) {
+					return Integer.compare(map.get(s2), map.get(s1));
 				}
-				//단어의 길이가 길수록 앞에 배치
 				if(s1.length()!=s2.length()) {
 					return s2.length()-s1.length();
 				}
-				//알파벳 사전 순으로 배치
 				return s1.compareTo(s2);
 			}
 		};
 		list.sort(comparator);
 		
-		for(String str:list) {
+		for(String str: list) {
 			sb.append(str+"\n");
 		}
 		System.out.println(sb);
