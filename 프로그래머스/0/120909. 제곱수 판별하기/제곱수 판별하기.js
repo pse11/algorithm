7e34cb76cd1d529f81420b69f3f36ea7e1d9392c
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-11-15 23:03:55 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    for(let i=1;i<=n;i++){
-        if(n/i==i){ //몫과 같을 때 제곱수로 판단
-            return 1;
-        }
-    }
-    return 2;
+    return Number.isInteger(Math.sqrt(n))?1:2;
 }
