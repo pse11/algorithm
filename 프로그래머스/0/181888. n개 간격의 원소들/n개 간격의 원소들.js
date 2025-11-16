a44153dc302170a331fd449f6554773524e93a3d
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-11-16 23:30:39 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,7 +1,1 @@
-function solution(num_list, n) {
-    var answer = [];
-    for(let i=0;i<num_list.length;i+=n){
-        answer.push(num_list[i]);
-    }
-    return answer;
-}
+const solution = (num_list, n) => num_list.filter((_,i)=>!(i%n));
