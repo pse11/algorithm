0a7a6d21238386748fa03d4f5006eb9e83fac1d6
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-11-19 23:15:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    let arr = my_string.split("");
+    let tmp = arr[num1];
+    arr[num1]=arr[num2];
+    arr[num2]=tmp;
+    my_string = arr.join('');
+    return my_string;
+}
