d23e3f826b21f315ec4665671d2c5dd454128b23
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-12-13 23:19:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.79 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,20 +1,17 @@
 function solution(participant, completion) {
     let map = new Map();
-    //참여자 해시 추가
+    
     for(let i=0;i<participant.length;i++){
-        if(map.has(participant[i])){ //동명이인 여부
-            map.set(participant[i],map.get(participant[i])+1);
-        }else{
-            map.set(participant[i],1);
-        }
-    }
-    //완주자 해시 수정
-    for(let i=0;i<completion.length;i++){
-        map.set(completion[i],map.get(completion[i])-1);
+        let a =  participant[i];
+        let b = completion[i];
         
+        map.set(a, (map.get(a)||0)+1);
+        map.set(b, (map.get(b)||0)-1);
     }
-    //미완주자 찾기()
-    for(const [name, value] of map){
-        if(value>0) return name;
+    
+    for(let [name, value] of map){
+        if(value>0){
+            return name;
+        }
     }
 }
