79fd867b1a600533ddb5f2e280c406f520076e49
Sangeun Park <102156161+pse11@users.noreply.github.com>
2025-12-16 12:57:25 +0900

    Update 완주하지 못한 선수.js

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,11 @@
 function solution(participant, completion) {
     let map = new Map();
     
-    for(let i=0;i<participant.length;i++){
+    for(let i=0;i<participant.length;i++){ //자바스크립트는 index 에러가 나지 않음. undefined를 return.
         let a =  participant[i];
         let b = completion[i];
         
-        map.set(a, (map.get(a)||0)+1);
+        map.set(a, (map.get(a)||0)+1); //자바스크립트에서 undefined는 falsy이기 때문에 map에 key가 없는 경우 undefined로 false -> 0으로 set
         map.set(b, (map.get(b)||0)-1);
     }
     
