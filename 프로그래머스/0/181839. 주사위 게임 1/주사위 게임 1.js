9b1fc28ca46c344ad32e0682b447bc6a9b680865
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-03 17:00:47 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,9 +1,9 @@
 function solution(a, b) {
-    if(a%2===1&&b%2===1){ //모두 홀수
-         return a*a+b*b;
-    }else if(a%2===0&&b%2===0){ //모두 홀수 x
-        return Math.abs(a-b);
-    }else{ // 하나만 홀수
-        return 2*(a+b);
-    }
+    //삼항 연산자로 풀이 
+    const isOdd = (num) => num%2===1; //홀수 여부 확인 함수
+    
+    return isOdd(a)&&isOdd(b) ? a*a+b*b 
+        : !isOdd(a)&&!isOdd(b) ? Math.abs(a-b) 
+        : 2 * (a+b);
+    
 }
