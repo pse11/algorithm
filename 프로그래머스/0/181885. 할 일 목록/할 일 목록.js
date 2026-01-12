3950251efa34aaf6092a449e2a6023385381fe94
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-12 11:18:56 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,3 @@
 function solution(todo_list, finished) {
-    let result = [];
-    for(let i=0;i<todo_list.length;i++){
-        if(finished[i]===false){
-            result.push(todo_list[i]);
-        }
-    }
-    return result;
+    return todo_list.filter((e,i)=>!finished[i]);
 }
