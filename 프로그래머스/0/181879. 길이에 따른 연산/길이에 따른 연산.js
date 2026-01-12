32533372484d299b4593ddb5fe326009c60a9d7f
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-12 11:05:54 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,12 +1,8 @@
 function solution(num_list) {
-    if(num_list.length>=11){
-        return num_list.reduce((acc,cur)=>{
-            return acc+cur;
-        },0);
-    }else if(num_list.length<=10){
-        return num_list.reduce((acc,cur)=>{
-            return acc*cur;
-        },1);
-    }
+    let mul = (acc, cur) => acc*cur;
+    let add = (acc, cur) => acc+cur;
     
+    return num_list.length>10?
+        num_list.reduce(add,0):
+        num_list.reduce(mul,1);
 }
