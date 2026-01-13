56f8eb5213eca8125460a6d0bbaca15daa97fb73
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-13 13:54:14 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,13 +1,3 @@
 function solution(myString) {
-   return myString.split("").map((v)=>{
-       if(v==="a"){
-           return "A";
-       }else if(v==="A"){
-           return "A";
-       }else{
-           return v.toLowerCase();
-           
-       }
-   }).join("");
-    
+   return myString.toLowerCase().replaceAll("a","A");
 }
