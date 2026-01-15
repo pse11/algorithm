4dfb9149ec6b855be7ba53268db77d778f6bd253
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-15 10:32:39 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,3 +1,12 @@
 function solution(n) {
-    return [...String(n)].reverse().map((v)=>+v);
+    //문자풀이
+    //return [...String(n)].reverse().map((v)=>+v);
+    
+    //숫자 풀이
+    let arr = [];
+    do{
+        arr.push(n%10);
+        n=Math.floor(n/10);
+    }while(n>0);
+    return arr;
 }
