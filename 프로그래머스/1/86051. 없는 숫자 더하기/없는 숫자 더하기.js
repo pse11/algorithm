c9a96c60a66341ef062c18c6aa04a9d73762d1e4
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-15 10:54:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,3 +1,9 @@
 function solution(numbers) {
-    return 45-numbers.reduce((acc,cur)=>acc+cur,0);
+    //return 45-numbers.reduce((acc,cur)=>acc+cur,0);
+    
+    let sum = 0;
+    for(let i=0;i<=9;i++){
+        !numbers.includes(i)?sum+=i:sum+=0;
+    }
+    return sum;
 }
