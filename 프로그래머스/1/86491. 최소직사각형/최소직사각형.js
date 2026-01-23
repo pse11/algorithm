b8a7f0f70564ca15a7800f0f38f580a74e3488cc
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-23 17:21:11 +0900

    [level 1] Title: 최소직사각형, Time: 38.93 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,14 @@
 function solution(sizes) {
     //긴 변을 가로로 몰아넣고
     //가로 중 최대 x 세로 중 최소 
-    let maxWidth = 0;
-    let minHeight = 0;
-
-    sizes.forEach(item=>{
-        const width = Math.max(item[0],item[1]);
-        const height = Math.min(item[0],item[1]);
-        
-        if(width > maxWidth) maxWidth = width;
-        
-        if(height>minHeight) minHeight = height;
-    });
+    const sortArr = sizes.map(([w,h])=>w<h? [h,w] : [w,h]);
     
-    return maxWidth*minHeight;
+    let maxSet = [0,0];
+    
+    sortArr.forEach(([w,h])=>{
+        if( w > maxSet[0] ) maxSet[0] = w;
+        if( h > maxSet[1] ) maxSet[1] = h;
+    })
+    
+    return maxSet[0]*maxSet[1];
 }
