28f4eeee58cc773ffbc1634aa29b7ffe67594733
Sangeun Park <102156161+pse11@users.noreply.github.com>
2026-01-27 14:40:16 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 4.06 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,9 +1,4 @@
 function solution(arr, queries) {
-    for(let i=0;i<queries.length;i++){
-        const [s,e] = queries[i];
-        for(let j=s;j<=e;j++){
-            arr[j]++;
-        }
-    }
-    return arr;
+    var answer = [];
+    return answer;
 }
